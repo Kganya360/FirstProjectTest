2234f2e15ad347e163a793692501f6cb3f59ba93
kgany <kgany@Kganya360->
2024-09-09 09:29:28 +0200

    first commit

---
+++ src/main/java/com/mycompany/githubfirstproject/GitHubFirstProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubfirstproject;
+
+/**
+ *
+ * @author kgany
+ */
+public class GitHubFirstProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
